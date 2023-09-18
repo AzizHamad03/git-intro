8634c252bfb8909dcc55851efb73c19db15effb1
AzizHamad <s202183050@kfupm.edu.sa>
2023-09-18 16:49:16 +0300

    adding selection sort

--- Sorter.java
+++ Sorter.java
@@ -1,6 +1,21 @@
 //push test  
-class lab03{
+class lab03 {
     public static void main(String[] args) {
         System.out.println("test");
+    }
+
+    void selectionSort(int[] array) {
+        int len = array.length;
+        for (int i = 0; i < len - 1; i++) {
+            
+            int smallest = i;
+            for (int j = i + 1; j < len; j++)
+                if (array[j] < array[smallest])
+                    smallest = j;
+
+            int temp = array[smallest];
+            array[smallest] = array[i];
+            array[i] = temp;
+        }
     }
 }
