67a45360853645da52b58c8d782ed51bc6980cc0
SauceX <mahmoudzak4@gmail.com>
2023-09-18 17:55:52 +0300

    feat: insertion sort method

--- Sorter.java
+++ Sorter.java
@@ -7,7 +7,7 @@ class lab03 {
     void selectionSort(int[] array) {
         int len = array.length;
         for (int i = 0; i < len - 1; i++) {
-            
+
             int smallest = i;
             for (int j = i + 1; j < len; j++)
                 if (array[j] < array[smallest])
@@ -16,6 +16,21 @@ class lab03 {
             int temp = array[smallest];
             array[smallest] = array[i];
             array[i] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; ++i) {
+            int key = array[i];
+            int j = i - 1;
+
+            // Move elements of array[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
         }
     }
 }
